42eefb27b04a10972e8aa44ddabdf21e916d5cd9
cicko00 <101884541+cicko00@users.noreply.github.com>
2022-05-28 19:06:37 +0200

    Add files via upload

---
+++ res-tim19-master/Lokalni Kontroler/Lokalni_Kontroler.py
@@ -0,0 +1,30 @@
+
+
+
+import socket
+import kontroler_funkcije
+ 
+print("Kontroler pocinje sa radom!");
+print()
+print()
+
+kontroler_funkcije.Konekcija()
+
+
+
+
+
+        
+        
+
+
+
+    
+    
+
+
+
+
+
+
+
