65e6d7139c575432a2aaa17e01de2eb8f0521eb8
EmiliaBalaz <emiliabalazs.ki@gmail.com>
2022-05-29 14:33:40 +0200

    Added xml storage
    Added xml storage to local controller.

---
+++ Lokalni Kontroler/Model/LocalDevice.py
@@ -0,0 +1,5 @@
+class LocalDevice:
+    def __init__(self, deviceCode, time, value):
+        self.localDeviceCode = deviceCode
+        self.timeStamp = time
+        self.actualValue = value
---
+++ Lokalni Kontroler/Model/LocalDeviceStorage.py
@@ -0,0 +1,16 @@
+from Model.LocalDevice import LocalDevice
+from Common.XMLWritter import XMLWritter
+
+
+class LocalDeviceStorage:
+    def __init__(self):
+        self.deviceValues = []
+
+    def AddNewDeviceValue(self, deviceValue:LocalDevice):
+        self.deviceValues.append(vars(deviceValue))
+        values = {'DeviceValues': {'LocalDevice': self.deviceValues}}
+
+        xmlWritter = XMLWritter(values)
+
+        f = open("deviceValues.xml", "w")
+        f.write(xmlWritter.doc.toxml("UTF-8").decode("UTF=8"))
