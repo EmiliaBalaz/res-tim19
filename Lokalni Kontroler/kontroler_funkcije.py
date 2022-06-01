e5ebc58fdd6a58ac7f2363f113a7feb239e69771
EmiliaBalaz <emiliabalazs.ki@gmail.com>
2022-06-01 20:37:03 +0200

    Biranje uredjaja i menjanje vrednosti
    Korisniku se pruza mogucnost da izabere analogni ili digitalni uredjaj. Zatim da unese podatke, da ih posalje i da promeni vrednost i ponovo posalje kontroleru. U kontroleru se svaka promena cuva u XML fajlu. Potrebno je jos dodati proveru u zavisnosti od tipa uredjaja da li je poslata odgovarajuca vrednost (za analogni uredjaj je to broj, za digitalni ON, OFF, OPEN, CLOSE).

--- Lokalni Kontroler/kontroler_funkcije.py
+++ Lokalni Kontroler/kontroler_funkcije.py
@@ -8,19 +8,16 @@ def Konekcija(localDeviceStorage:LocalDeviceStorage):
     server.bind(('localhost',50000))
     server.listen(10)
     print("--SERVER CEKA NOVE KONEKCIJE!--")
-    while(True):
+    while (True):
         client,client_address=server.accept();
         print()
         print()
         print("PODACI PRIMLJENI OD: "+str(client_address))
-        client.send(bytes("SERVER JE USPESNO PRIMIO PODATKE","utf-8"))
         temp= client.recv(1024);
         msg=temp.decode("utf-8")
         print(str(msg))
-
-        values = msg.split(",")
+        values = msg.split("/")
         localDeviceValue = LocalDevice(values[0], values[1], values[2])
-
         localDeviceStorage.AddNewDeviceValue(localDeviceValue)
         client.close()
         
