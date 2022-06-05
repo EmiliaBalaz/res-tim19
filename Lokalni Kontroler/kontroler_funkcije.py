de3fee09285e69acb530a5a7980ae2ca0b5c32e3
cicko00 <101884541+cicko00@users.noreply.github.com>
2022-06-05 23:49:44 +0200

    Omoguceno kontrolerima da na svakih 5 minuta salju poruku AMS-u.

--- Lokalni Kontroler/kontroler_funkcije.py
+++ Lokalni Kontroler/kontroler_funkcije.py
@@ -1,4 +1,5 @@
 import socket
+import time
 from Model.LocalDeviceStorage import LocalDeviceStorage
 from Model.LocalDevice import LocalDevice
 
@@ -19,6 +20,18 @@ def Konekcija(localDeviceStorage:LocalDeviceStorage,port):
         values = msg.split("/")
         localDeviceValue = LocalDevice(values[0], values[1], values[2])
         localDeviceStorage.AddNewDeviceValue(localDeviceValue)
+        client.close()
+        
+def Slanje_na_AMS(port):
+    while(True):
+        start=time.time()
+        while((time.time()-start)<=300):
+            pass
+        xx=time.time()-start
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(("localhost", port))
+        msg=str(xx)
+        client.send(bytes(msg, 'utf-8'))
         client.close()
         
         
