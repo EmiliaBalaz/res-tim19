44f2d3ddc4a6265e4cf4c6f33a01c819239440ff
cicko00 <101884541+cicko00@users.noreply.github.com>
2022-06-08 20:13:36 +0200

    Omoguciti ubrzavanje vremena

---
+++ AMS/TimeSim/TimeSim.py
@@ -0,0 +1,31 @@
+import time
+import xml.etree.ElementTree as ET
+
+def ReadSimTime():
+    lista=ET.parse("C:\\Users\\Cvijetin Glisic\\Desktop\\GIT_REPOZITORIJUM\\GIT\AMS\\TimeSim\\TimeSimConfig.xml")
+    root=lista.getroot()
+    a=int(root[0].text)
+    return a
+    
+
+
+
+class TimeSimulation():
+    time_str=0
+    
+    @staticmethod
+    def COUNT_START():
+        TimeSimulation.time_str=time.time()
+       
+
+   
+
+    @staticmethod
+    def TimePassed():
+        return (time.time()-TimeSimulation.time_str)*ReadSimTime()
+
+
+
+
+
+
