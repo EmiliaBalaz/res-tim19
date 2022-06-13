7460560e78f3373aa90e867d8c9436ec0733079f
EmiliaBalaz <emiliabalazs.ki@gmail.com>
2022-06-13 21:30:50 +0200

    Testiranje funkcija
    Testirane su tri funkcije, dve sluze za proveru podataka kada se unesi podaci za analogni i digitalni uredjaj, dok treca za unos porta. U svakom testiranju su uzeti u obzir i negativni slucajevi.

---
+++ LOKALNI_UREDJAJ/Testovi/test_Unos_porta.py
@@ -0,0 +1,38 @@
+import unittest
+import mock
+
+from LokalniUredjaj.Uredjaj_funkcije import SviKontroleri, Unos_porta
+
+
+class MyTestCase(unittest.TestCase):
+    def test_case1(self):
+        port = "50002"
+        with mock.patch('builtins.input', return_value=port):
+            unetiPort = Unos_porta()
+
+        self.assertEqual(unetiPort, port)
+
+    def test_case2(self):
+        port=""
+        with mock.patch('builtins.input', return_value=port):
+            unetiPort = Unos_porta()
+
+        self.assertEqual(unetiPort, "ERROR")
+
+    def test_case3(self):
+        port="12345"
+        with mock.patch('builtins.input', return_value=port):
+            unetiPort = Unos_porta()
+
+        self.assertEqual(unetiPort, "ERROR");
+
+    def test_case4(self):
+        port = "abc"
+        with mock.patch('builtins.input', return_value=port):
+            unetiPort = Unos_porta()
+
+        self.assertEqual(unetiPort, "ERROR");
+
+
+if __name__ == '__main__':
+    unittest.main()
