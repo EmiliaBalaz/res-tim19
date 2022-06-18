9e091ad3f3ca3ddf2cb54d80a1c684648bbb0c7f
EmiliaBalaz <101884328+EmiliaBalaz@users.noreply.github.com>
2022-06-18 21:09:36 +0200

    Update AMSDatabaseInitialization.sql

--- Database/AMSDatabaseInitialization.sql
+++ Database/AMSDatabaseInitialization.sql
@@ -4,3 +4,10 @@ create table LocalDevice(
 	Vreme datetime NOT NULL,
     	ActualValue int NOT NULL,
     	constraint localdevice_pk primary key (id));
+	
+create table LocalDeviceDigital(
+	id int NOT NULL AUTO_INCREMENT,
+	DeviceCode varchar(50) NOT NULL , 
+	Vreme datetime NOT NULL,
+    	ActualValue varchar(50) NOT NULL,
+    	constraint localdevice_pk primary key (id));
