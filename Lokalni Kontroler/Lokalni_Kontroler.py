56c7eecec159ad85821e087063162c59fbabf876
EmiliaBalaz <101884328+EmiliaBalaz@users.noreply.github.com>
2022-06-19 15:31:09 +0200

    Nova grana (#29)
    * Novosti
    
    * Semaphore dodat za pisanje i citanje

--- Lokalni Kontroler/Lokalni_Kontroler.py
+++ Lokalni Kontroler/Lokalni_Kontroler.py
@@ -39,9 +39,10 @@ if __name__ == "__main__":
     print()
     atexit.register(kontroler_funkcije.Exit_Handler,port,naz)
     deviceStorage = LocalDeviceStorage()
-    y=threading.Thread(target=kontroler_funkcije.Konekcija,args=(deviceStorage,int(port), naz),daemon=True)
+    semaphore = threading.Semaphore(0)
+    y=threading.Thread(target=kontroler_funkcije.Konekcija,args=(deviceStorage,int(port), naz, semaphore),daemon=True)
     y.start()
-    x=threading.Thread(target=kontroler_funkcije.Slanje_na_AMS,args=(50015,port, naz),daemon=True)
+    x=threading.Thread(target=kontroler_funkcije.Slanje_na_AMS,args=(50015,port, naz, semaphore),daemon=True)
     x.start()
    
     kontroler_funkcije.Izadji_Iz_Aplikacije()
